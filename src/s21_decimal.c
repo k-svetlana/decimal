2974073c079e622f0d33b1e84d22dbf17bf8f6fb
Svetlana <112399684+k-svetlana@users.noreply.github.com>
2025-03-10 23:09:43 +0300

    Update s21_is_equal

--- src/s21_decimal.c
+++ src/s21_decimal.c
@@ -82,24 +82,21 @@ int s21_is_equal(s21_decimal *leftOp, s21_decimal *rightOp) {
   
   if (!leftOp || !rightOp) {
     isEqual = FALSE;
-  }
-
-  int leftIsZero = (leftOp->bits[0] == 0 && leftOp->bits[1] == 0 && leftOp->bits[2] == 0);
-  int rightIsZero = (rightOp->bits[0] == 0 && rightOp->bits[1] == 0 && rightOp->bits[2] == 0);
-
-  if (leftIsZero && rightIsZero) {
-    isEqual = TRUE;
-  } else if (getSign(*leftOp) != getSign(*rightOp)) {
-    isEqual = FALSE;
   } else {
-    s21_normalize(leftOp, rightOp);
-    for (int bitsIndex = 0; bitsIndex < 4; bitsIndex++) {
-      if (leftOp->bits[bitsIndex] != rightOp->bits[bitsIndex]) {
-        isEqual = FALSE;
+    int leftIsZero = (leftOp->bits[0] == 0 && leftOp->bits[1] == 0 && leftOp->bits[2] == 0);
+    int rightIsZero = (rightOp->bits[0] == 0 && rightOp->bits[1] == 0 && rightOp->bits[2] == 0);
+
+    if (leftIsZero && rightIsZero) {
+      isEqual = TRUE;
+    } else {
+      s21_normalize(leftOp, rightOp);
+      for (int bitsIndex = 0; bitsIndex < 4; bitsIndex++) {
+        if (leftOp->bits[bitsIndex] != rightOp->bits[bitsIndex]) {
+          isEqual = FALSE;
+        }
       }
     }
   }
-  
   return isEqual;
 }
 
